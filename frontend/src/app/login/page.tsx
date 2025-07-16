98c692acdb7fe5955f94404f779f27a79daf76ec
Ketan <ketan.agarwal2506@gmail.com>
2025-07-16 09:42:18 +0530

    fix: login

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -33,6 +33,7 @@ export default function LoginPage() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
+        credentials: 'include'
       });
       if (res.status === 422) {
         toast.error("Please recheck your login details.", {
