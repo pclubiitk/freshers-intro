e32de960471e4df4f975de25cc9f877724e20721
Ketan <ketan.agarwal2506@gmail.com>
2025-07-20 04:44:23 +0530

    fix: envs

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+print("DATABASE_URL:", os.getenv("DATABASE_URL"))
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 Base = declarative_base()
